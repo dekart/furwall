02ea77622aff1221547d68120c3789b9bb1aa63d
DEkart <dekart@Lamptop.(none)>
2009-01-10 14:56:17 +0500

    Modules should have very own permission management

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 require File.join(File.dirname(__FILE__), "lib", "furwall", "access_handler")
-require File.join(File.dirname(__FILE__), "lib", "furwall", "active_record_permissions")
 require File.join(File.dirname(__FILE__), "app", "models", "permission")
 
 Object.send(:include, Furwall::ObjectPermissions)
+Module.send(:include, Furwall::ModulePermissions)
 ActiveRecord::Base.send(:include, Furwall::ActiveRecordPermissions)
+
 ActionController::Base.send(:include, Furwall::ControllerExtensions)
